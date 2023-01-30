767e528cc415251a09afd28b47236b27bffa73c2
Nekrocow <nekrocow@gmail.com>
2023-01-30 01:50:35 -0300

    fix/working solution according to instructions

--- Activities/SevenDisplay.js
+++ Activities/SevenDisplay.js
@@ -1,14 +1,26 @@
 const { converter } = require('./converterDictionary')
 
+// La solución está MAL porque tengo que hacer el número línea por línea, no número por número; revisar
+
 function print(number) {
 	const digits = Array.from(number.toString(), Number)
 
+	let upperLine = ""
+	let midLine = ""
+	let bottomLine = ""
+
 	for (const num of digits) {
-		converter[num].forEach(e => {
-			console.log(e)
-		})
+		upperLine += converter[num][0]
+		midLine += converter[num][1]
+		bottomLine += converter[num][2]
 	}
+
+	console.log(upperLine)
+	console.log(midLine)
+	console.log(bottomLine)
 	return
 }
 
 module.exports = print
+
+print(12)
